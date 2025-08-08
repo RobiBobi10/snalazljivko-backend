814e03f70aa06a64e1b1e185ac96bb82288a9d2a
RobiBobi10 <93741172+RobiBobi10@users.noreply.github.com>
2025-08-08 22:46:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "API radi!"}
-
-git add main.py
-git commit -m "Dodao osnovnu FastAPI aplikaciju"
-git push
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "API radi! Dobrodošao na Snalazljivko backend 🚀"}
+    return {"message": "API radi! Dobrodošao na Snalazljivko backend 🎒"}
